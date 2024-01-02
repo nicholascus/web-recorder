fe5bad3b5810155fe230c0e4246636c239aac620
Niko G <1756853+nicholascus@users.noreply.github.com>
2024-01-03 04:33:37 +1100

    introduce bootstrap class

---
+++ src/engine/Bootstrap.ts
@@ -0,0 +1,81 @@
+import { Browser, BrowserContext, Page, chromium } from "playwright";
+import ComponentLoader from "./ComponentLoader";
+import URI from "urijs";
+import IWebParser from "../base/IWebParser";
+
+export class Bootstrap {
+    componentLoader: ComponentLoader = new ComponentLoader();
+    a: Promise<Page>[] = [];
+
+    async attachParsers(page: Page) {
+        try {
+            await page.bringToFront();
+            const title: string = await page.title();
+            const pageurl: string = page.url();
+            console.log(`Page found. title: ${title} url:${pageurl}`);
+            const uri = new URI(pageurl);
+    
+            const parsers: IWebParser[] = await this.componentLoader.getParsers();
+            
+            for(var i=0; i < parsers.length; i++) {
+                const parser = parsers[i];
+                if (parser.isParsableUrl(uri)) {
+                    await parser.parsePage(page);                
+                    page.on('request', async (...args) => {
+                        await parser.parsePage(page);                
+                    });
+                    this.a.push(page.waitForEvent('close', {
+                        timeout: 0,
+                        predicate: (p: Page) => {
+                            console.log(`!!PAGE CLOSED!! ${title}`);
+                            return true;
+                        }
+                    }));
+                }
+            }
+        } catch (e) {
+            console.log(`failed to get page title due to ${e}`)
+        }
+    }
+    
+    async attachBrowser(browser: Browser) {
+        const contexts: BrowserContext[] = browser.contexts();
+    
+        for(let j=0; j<contexts.length; j++) {
+            const context: BrowserContext = contexts[j];
+            context.setDefaultTimeout(1000);
+            const pages: Page[] = context.pages();
+            console.log(`!!CONTEXT!! N${j}`);
+            context.on('page', (page) => {
+                console.log('!!NEW PAGE!! need to register to tab list');
+                page.on('domcontentloaded', async (page) => {
+                    console.log(await page.title());
+                    await this.attachParsers(page);
+                });
+            });
+    
+            for(let i=0; i<pages.length; i++) {
+                const page: Page = pages[i];
+                await this.attachParsers(page);
+            }
+        }
+    }
+
+    async run() {
+        try {
+            const browser: Browser = await chromium.connectOverCDP('http://localhost:9222');
+            // browser.on('disconnected', () => process.exit());
+    
+            console.log(browser.isConnected() && 'Connected to Chrome.');
+            console.log(`Contexts in CDP session: ${browser.contexts().length}.`);
+    
+            await this.attachBrowser(browser);
+    
+            await Promise.all(this.a);
+    
+            await browser.close();
+        } catch (error) {
+            console.log(`Cannot connect to Chrome.\n${error}`);
+        }            
+    }
+}
--- src/start-playwright.ts
+++ src/start-playwright.ts
@@ -28,86 +28,11 @@
  * 
  */
 
-import { Browser, BrowserContext, Locator, Page, chromium } from 'playwright';  // Or 'firefox' or 'webkit'.
-import URI from 'urijs';
-import { TwitterFeedParser, TweetConsoleLogger } from './parsers/TwitterFeedParser';
-import { LinkedInJobCollectionsParser, LinkedInJobConsoleLogger } from './parsers/LinkedInJobCollectionsParser';
-import IWebParser from './base/IWebParser';
-import ComponentLoader from './engine/ComponentLoader';
-
-const componentLoader = new ComponentLoader();
-
-async function attachParsers(page: Page) {
-    try {
-        await page.bringToFront();
-        const title: string = await page.title();
-        const pageurl: string = page.url();
-        console.log(`Page found. title: ${title} url:${pageurl}`);
-        const uri = new URI(pageurl);
-
-        const parsers: IWebParser[] = await componentLoader.getParsers();
-        
-        for(var i=0; i < parsers.length; i++) {
-            const parser = parsers[i];
-            if (parser.isParsableUrl(uri)) {
-                await parser.parsePage(page);                
-                page.on('request', async (...args) => {
-                    await parser.parsePage(page);                
-                });
-                a.push(page.waitForEvent('close', {
-                    timeout: 0,
-                    predicate: (p: Page) => {
-                        console.log(`!!PAGE CLOSED!! ${title}`);
-                        return true;
-                    }
-                }));
-            }
-        }
-    } catch (e) {
-        console.log(`failed to get page title due to ${e}`)
-    }
-}
-
-
-const a = [];
+import { Bootstrap } from './engine/Bootstrap';
 
 (async () => {
-    try {
-        const browser: Browser = await chromium.connectOverCDP('http://localhost:9222');
-        // browser.on('disconnected', () => process.exit());
-
-        console.log(browser.isConnected() && 'Connected to Chrome.');
-        console.log(`Contexts in CDP session: ${browser.contexts().length}.`);
-
-        const contexts: BrowserContext[] = browser.contexts();
-
-        for(let j=0; j<contexts.length; j++) {
-            const context: BrowserContext = contexts[j];
-            context.setDefaultTimeout(1000);
-            const pages: Page[] = context.pages();
-            console.log(`!!CONTEXT!! N${j}`);
-            context.on('page', (page) => {
-                console.log('!!NEW PAGE!! need to register to tab list');
-                page.on('domcontentloaded', async (page) => {
-                    console.log(await page.title());
-                    await attachParsers(page);
-                });
-            });
-
-            for(let i=0; i<pages.length; i++) {
-                const page: Page = pages[i];
-                await attachParsers(page);
-            }
-        }
-
-        await Promise.all(a);
-
-        await browser.close();
-    } catch (error) {
-        console.log(`Cannot connect to Chrome.\n${error}`);
-    }
+    await new Bootstrap().run();
 })();
-
 
 // profile  ~/Library/Application Support/Google/Chrome
 // binary  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222
